87bc5f5bef77274f59985bea2c761b6495fd3d43
unknown <nyhchang222@gmail.com>
2019-04-25 22:07:13 -0700

    Missing basemap data file, route to stmflow is wrong

---
+++ mapTools.py
@@ -0,0 +1,8 @@
+from qgis.core import *
+from qgis.gui import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from constants import *
+
+
---
+++ python-qgis-pycharm.bat
@@ -0,0 +1,16 @@
+@echo off
+call "C:\OSGeo4W64\bin\o4w_env.bat"
+call "C:\OSGeo4W64\bin\qt5_env.bat"
+call "C:\OSGeo4W64\bin\py3_env.bat"
+@echo off
+set OSGEO4W_ROOT=C:\OSGeo4W64
+path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
+set GDAL_FILENAME_IS_UTF8=YES
+rem Set VSI cache to be used as buffer, see #6448
+set VSI_CACHE=TRUE
+set VSI_CACHE_SIZE=1000000
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;%PYTHONPATH%
+
+"C:\Program Files\JetBrains\PyCharm2018\bin\pycharm64" %*
